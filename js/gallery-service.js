3c4478149337de16c3f8a63607bc8168c21e8850
Jonathan Geiger <tantangeiger@gmail.com>
2021-08-02 17:35:30 +0300

    Added projects

--- js/gallery-service.js
+++ js/gallery-service.js
@@ -2,14 +2,14 @@
 var gProjs = [];
 createProjects();
 
-function createProject(name, title, desc, url, lables, img) {
+function createProject(name, title, desc, url, date, lables, img) {
   var proj = {
     id: makeId(6),
     name: name,
     title: title,
     desc: desc,
     url: url,
-    publishedAt: Date().toString(),
+    publishedAt: date,
     labels: lables,
     img: img,
   };
@@ -24,8 +24,20 @@ function createProjects() {
       'Arcade',
       'Fun clone of the original Minesweeper game',
       'https://geiger01.github.io/Minesweeper_js/',
+      '20.07.21',
       ['gaming', ' vanilla js'],
       '../img/portfolio/minesweeper.png'
+    )
+  );
+  gProjs.push(
+    createProject(
+      'Book Store',
+      'Data-Storage',
+      'Book store website to keep track of books',
+      'https://elastic-hoover-3f8c32.netlify.app/',
+      '30.07.21',
+      ['CRUD', ' MVC', ' vanilla js', ' local-storage'],
+      '../img/portfolio/book.png'
     )
   );
 
@@ -35,8 +47,20 @@ function createProjects() {
       'Arcade',
       'Fun clone of the game Pacman',
       'https://vigilant-volhard-00960b.netlify.app/',
+      '19.07.21',
       ['gaming', ' vanilla js'],
       '../img/portfolio/pacman.png'
+    )
+  );
+  gProjs.push(
+    createProject(
+      'Calculator',
+      'Math',
+      '',
+      'https://silly-lamport-6ffff8.netlify.app/',
+      '16.07.21',
+      ['vanilla js'],
+      '../img/portfolio/calc.png'
     )
   );
 
@@ -46,8 +70,31 @@ function createProjects() {
       'Arcade',
       'Touch the numbers as fast as you can',
       'https://relaxed-austin-52d2a2.netlify.app/',
-      ['gaming', ' vanilla js'],
+      '13.07.21',
+      ['gaming', ' vanilla js', ' local-storage'],
       '../img/portfolio/nums.png'
+    )
+  );
+  gProjs.push(
+    createProject(
+      'Safe Content',
+      'Login-auth',
+      'login authentication page',
+      'https://amazing-thompson-6fd20f.netlify.app/secret.html',
+      '28.07.21',
+      ['MVC', ' vanilla js'],
+      '../img/portfolio/log.png'
+    )
+  );
+  gProjs.push(
+    createProject(
+      'Picture Quiz',
+      'Quiz',
+      'Quiz game website',
+      'https://optimistic-galileo-907c6e.netlify.app/',
+      '17.07.21',
+      ['Arcade', ' vanilla js'],
+      '../img/portfolio/quiz.png'
     )
   );
 }
--- js/main.js
+++ js/main.js
@@ -96,5 +96,5 @@ function renderModals() {
     `);
   });
 
-  $('.boolbil').html(strHtmls);
+  $('.modals-section').html(strHtmls);
 }
